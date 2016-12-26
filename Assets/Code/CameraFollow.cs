760ad0579b3611f62894cf662558369123cde4e3
Dylan Ilvento <dylanilvento@gmail.com>
2016-12-25 23:45:57 -0500

    Working on parallax scrolling for background mountains.

--- Assets/Code/CameraFollow.cs
+++ Assets/Code/CameraFollow.cs
@@ -8,6 +8,10 @@ public class CameraFollow : MonoBehaviour {
 	Vector2 relPos;
 	Camera camera;
 	public bool canMove = true;
+
+
+	float upVal = 3f;
+	float downVal = 4f;
 	// Use this for initialization
 	void Start () {
 		player = target.transform;
@@ -19,19 +23,19 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3(player.position.x - relPos.x, transform.position.y, transform.position.z);
-		if (camera.WorldToScreenPoint(player.position).y > Screen.height / 1.5f) StartCoroutine("MoveUp");
-		else if (camera.WorldToScreenPoint(player.position).y < Screen.height / 1.75f) StartCoroutine("MoveDown");
+		if (camera.WorldToScreenPoint(player.position).y > Screen.height / upVal) StartCoroutine("MoveUp");
+		else if (camera.WorldToScreenPoint(player.position).y < Screen.height / downVal) StartCoroutine("MoveDown");
 	}
 
 	IEnumerator MoveUp () {
-		while (camera.WorldToScreenPoint(player.position).y > Screen.height / 1.5f && canMove) {
+		while (camera.WorldToScreenPoint(player.position).y > Screen.height / upVal && canMove) {
 			transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
 			yield return new WaitForSeconds(0.1f);
 		}
 	}
 
 	IEnumerator MoveDown () {
-		while (camera.WorldToScreenPoint(player.position).y < Screen.height / 4f && canMove) {
+		while (camera.WorldToScreenPoint(player.position).y < Screen.height / downVal && canMove) {
 			transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
 			yield return new WaitForSeconds(0.1f);
 		}
---
+++ Assets/Code/RollingMountains.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollingMountains : MonoBehaviour {
+
+	GameObject camera;
+	float relPosY;
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		// QualitySettings.vSyncCount = 0;
+		camera = GameObject.Find("Main Camera");
+		relPosY = camera.transform.position.y - transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector2 (transform.position.x + moveSpeed, transform.position.y);
+		if (transform.position.y < camera.transform.position.y - relPosY) StartCoroutine("MoveUp");
+
+	}
+
+	IEnumerator MoveUp () {
+		// while (transform.position.y < camera.transform.position.y - relPosY) {
+			transform.position = new Vector2(transform.position.x, transform.position.y + 0.01f);
+			yield return new WaitForSeconds(0.001f);
+		// }
+	}
+}
